be503d72ae6792e4114af1772ad637b2f7bb2e49
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-08-23 19:53:34 -0300

    add CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 @Module({
-  imports: [],
+  imports: [UsersModule,
+    MongooseModule.forRoot(process.env.MONGO)],
   controllers: [AppController],
   providers: [AppService],
 })
